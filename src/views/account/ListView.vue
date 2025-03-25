c548107c288b31bcf372ce05f82093dd4f71be89
zhy2658 <2131561293@qq.com>
2025-03-25 22:35:26 +0800

    新增登录认证

--- src/views/account/ListView.vue
+++ src/views/account/ListView.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
+  <div class="content-box">
+    <el-row :gutter="10" justify="center">
+      <el-col :sm="24" :lg="16">
+        <h4 v-if="userInfo.username">你好，{{ userInfo.username }}!</h4>
+        <el-form :model="loginForm" label-width="auto">
+          <el-row :gutter="10" justify="start">
+            <el-col :sm="10">
+              <el-form-item label="账号">
+                <el-input v-model="loginForm.username" />
+              </el-form-item>
+            </el-col>
+            <el-col :sm="10">
+              <el-form-item label="密码">
+                <el-input type="password" v-model="loginForm.password" />
+              </el-form-item>
+            </el-col>
+            <el-col :sm="4">
+              <el-form-item>
+                <el-button type="success" @click="onLogin()">登录</el-button>
+                <el-button type="danger" @click="onLogout()">注销</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+      </el-col>
+    </el-row>
     <el-row :gutter="10">
       <el-col :sm="6">
         <div class="grid-ctet grid-1">在！ 喵！ (立刻回复) wowo 又来啦！</div>
@@ -85,7 +110,14 @@
 </template>
 <script setup lang="ts">
   import { ref, reactive, watch, toRef, toRefs } from 'vue'
+  import { Base64 } from 'js-base64'
+  import { ElMessage } from 'element-plus'
   import axios from 'axios'
+
+  const baseUrl = 'https://api.w0w0.one'
+  const userInfo = ref({
+    username: ''
+  })
 
   let tableData = ref([
     {
@@ -103,16 +135,78 @@
     tel: '',
     email: ''
   })
+  const loginForm = reactive({
+    username: '',
+    password: ''
+  })
+
+  const token = localStorage.getItem('token')
+  if (token) {
+    getList(token)
+    const username = localStorage.getItem('username')
+    username && (userInfo.value.username = username)
+  }
 
   const onSubmit = () => {
     console.log('submit!')
   }
-
-  axios.post('https://api.w0w0.one/account/list').then(res => {
-    tableData.value = res.data
-  })
+  const onLogin = () => {
+    let { username, password } = loginForm
+    username = username.trim()
+    password = password.trim()
+    const code = Base64.encode(`${username}:${password}`)
+    const Authorization = `Basic ${code}`
+    console.log(Authorization)
+    axios
+      .post(`${baseUrl}/auth/login`, null, {
+        headers: {
+          Authorization
+        }
+      })
+      .then(res => {
+        ElMessage({
+          message: '登录成功了喵！',
+          type: 'success'
+        })
+        localStorage.setItem('username', res.data.username)
+        localStorage.setItem('token', res.data.token)
+        userInfo.value.username = res.data.username
+        getList(res.data.token)
+      })
+      .catch(res => {
+        ElMessage.error('抱歉喵~ 账号或密码好像输错了！')
+      })
+  }
+  function onLogout() {
+    localStorage.setItem('username', '')
+    localStorage.setItem('token', '')
+  }
+  function getList(token: string) {
+    axios
+      .post(`${baseUrl}/account/list`, null, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(res => {
+        tableData.value = res.data
+      })
+  }
 </script>
 <style scoped lang="scss">
+  .content-box {
+    padding: 1rem;
+    :deep(.el-form-item) {
+      margin: 10px 0;
+      .el-form-item__content {
+        display: flex;
+        justify-content: center;
+      }
+      button {
+        min-width: 200px;
+      }
+    }
+  }
   .grid-ctet {
     margin: 5px;
     background-color: rgba(0, 0, 0, 0.05);
